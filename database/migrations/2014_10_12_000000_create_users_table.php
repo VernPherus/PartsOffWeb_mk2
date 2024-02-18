9cbded1f359dc89b6af617bad6d2300fb8e63fc6
VernPherus <vernpherus@gmail.com>
2024-02-18 10:07:08 +0800

    added migrations and schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->date('date_of_birth');
+            $table->string('address1');
+            $table->string('address2');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
