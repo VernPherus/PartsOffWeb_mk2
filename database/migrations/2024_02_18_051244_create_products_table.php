8801400e6b909132e9b387933b3c4bc93c28b4a6
VernPherus <vernpherus@gmail.com>
2024-02-18 14:48:33 +0800

    added controllers, factories, seeders and resources for products and categories

---
+++ database/migrations/2024_02_18_051244_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->mediumText('description');
+            $table->decimal('price', 10, 2);
+            $table->enum('stock_status', ["instock", "outofstock"]);
+            $table->unsignedInteger('quantity')->default(1);
+            $table->string('image');
+            $table->text('images');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
